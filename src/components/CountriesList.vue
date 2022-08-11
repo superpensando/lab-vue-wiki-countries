3695a26e6dab80aa384d9ed9aec6dd8f44c25b72
laroca <laroca73@gmail.com>
2022-08-11 09:23:36 +0200

    Iteration 1.3 & Iteration 2 & Iteration 3 & Iteration 4:Bonus

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,14 +1,45 @@
 <template>
-  Country List
+  <ul class="list-group">
+    <li class="list-group-item">
+      <img class="country__image" :src="'https://flagcdn.com/w320/'+this.flagsLowercase+'.png'" />
+      <p class="country__name">{{ country.name.common }} - {{ flagsLowercase }} </p>
+    </li>
+  </ul>
 </template>
-
++
 <script>
 export default {
-    name: 'CountriesList',
+  name: 'CountriesList',
+  props: {
+    country: Object
+  },
+  data() {
+        return {
+            flags: this.country.alpha2Code,
+            flagsLowercase: "",
+        }
+  },
+  methods: {
+     lowercase() {
+       this.flagsLowercase=this.flags.toLowerCase();
+     }
+  },
+  mounted: function () {
+        this.$nextTick(function () {
+            this.lowercase();
+        })
+    }
 }
 
 </script>
 
 <style>
-
+.list-group-item {
+  text-align: center;
+}
+.country__image {
+  width:100%;
+  max-width: 100px;
+  display:inline-block;
+}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,38 @@
 <template>
-    <CountriesList />
+    <CountriesList v-for="country in countries" :country="country" :key="country.id" />
 </template>
 
 <script>
 import CountriesList from './../components/CountriesList.vue'
 export default {
+    data() {
+        return {
+            countries: []
+        }
+    },
     components: {
         CountriesList,
+    },
+    methods: {
+        async getCountries() {
+            const res = await fetch("https://ih-countries-api.herokuapp.com/countries");
+            const finalRes = await res.json();
+            finalRes.forEach(
+                (el) => {
+                    //console.log(el);
+                    this.countries.push(el);
+                }
+            );
+            console.log(this.countries);
+
+        }
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.getCountries()
+        })
     }
+
 }
 </script>
 
