58d853fc170ec13fffd524e9d9caad5f174d4d42
laroca <laroca73@gmail.com>
2022-08-12 19:53:13 +0200

    Iteration 1.4 & Iteration 5:Bonus

--- src/router.js
+++ src/router.js
@@ -1,23 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
+  
     {
       path: '/',
       name: 'root',
-      component: () => import(/* webpackChunkName: 'index' */ './pages/index.vue')
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: () => import(/* webpackChunkName: 'list' */ './components/CountriesList.vue'),
+      component: () => import(/* webpackChunkName: 'index' */ './pages/list.vue'),
       children: [
         {
-          path: '/details',
+          path: '/details/:id',
           name: 'details',
-          component: () => import(/* webpackChunkName: 'details' */ './components/CountriesDetails.vue')
+          component: () => import(/* webpackChunkName: 'details' */ './pages/details.vue')
         },
       ]
-    }
+    },
   ];
 
   const router = createRouter({
